aba5f294be495e392a23c95445967ab10c965833
Chloe Nuzillat <cnuzillat@gmail.com>
2025-11-27 17:58:35 -0500

    3105 Python solution

---
+++ Problems/3105/3105.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        inc = dec = ans = 1
+
+        for i in range(1, len(nums)):
+            if (nums[i] > nums[i - 1]):
+                inc += 1
+                dec = 1
+                ans = max(ans, inc)
+            elif (nums[i] < nums[i - 1]):
+                inc = 1
+                dec += 1
+                ans = max(ans, dec)
+            else:
+                inc = 1
+                dec = 1
+        
+        return ans
