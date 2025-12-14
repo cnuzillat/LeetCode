882d4bfc574dbe342177b2986147f043786f9e60
Chloe Nuzillat <cnuzillat@gmail.com>
2025-12-14 16:39:51 -0500

    Fix indentation in findErrorNums method

--- Problems/645/645.py
+++ Problems/645/645.py
@@ -1,5 +1,2 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        for i in range(0, len(nums)):
-            if (nums[i] == nums[i + 1]):
-                return [nums[i], nums[i] + 1]
