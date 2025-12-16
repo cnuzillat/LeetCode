a5d774c4ed0ee9557524449bd0c6148e88ffc737
Chloe Nuzillat <cnuzillat@gmail.com>
2025-12-15 19:46:54 -0500

    Implement findErrorNums to identify duplicates and missing

--- Problems/645/645.py
+++ Problems/645/645.py
@@ -1,2 +1,17 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
+        result = []
+
+        numsHash = {}
+
+        for num in nums:
+            if num in numsHash:
+                result.append(num)
+            else:
+                numsHash[num] = 1
+
+        for i in range (1, len(nums) + 1):
+            if i not in numsHash:
+                result.append(i)
+
+        return result
